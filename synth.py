5aa4d40684782f1b54aad6c70dae6192a88fa17f
Bu Sun Kim <busunkim@google.com>
2020-07-01 22:15:50 +0000

    chore: fix coverage

--- synth.py
+++ synth.py
@@ -357,7 +357,7 @@ s.replace(
 # ----------------------------------------------------------------------------
 # Add templated files
 # ----------------------------------------------------------------------------
-templated_files = common.py_library(unit_cov_level=78, cov_level=78)
+templated_files = common.py_library(cov_level=76)
 s.move(templated_files)
 
 # TODO(busunkim): Use latest sphinx after microgenerator transition
